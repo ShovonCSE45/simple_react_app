72d7b73b258a35a4b43832a9ef64b1a7fc4c5f08
ShovonCSE4501~ <shovon160145.com>
2023-09-29 12:11:42 +0600

    add local storage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {addToDb, getShoppingCart} from '../../utilities/fakedb'
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
@@ -12,10 +13,34 @@ const Shop = () => {
         .then(data => setProducts(data) )
     },[])
 
+    useEffect(()=> {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step 1: get id
+        for(const id in storedCart) {
+            // step 2: get the product using id
+            const addedProduct = products.find(product => product.id ===id)
+            if(addedProduct) {
+                // step 3: get quentity of the product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+
+                // step 4: add the added product to the saved cart
+                savedCart.push(addedProduct);
+
+            }  
+            console.log("added Products",addedProduct);     
+
+        }
+        setCart(savedCart);
+    } ,[products])
+
     const handleAddToCart = (product) => {
         // console.log("Product Added ok");
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
+        
 
     }
     return (
